5a5c39e099986bd415dd2c81470efeb8fee2feca
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-07-27 18:31:14 +0100

    Enhance location data structure by adding Arabic names for cities and regions; update processing logic to support new fields and create a city lookup map for improved access to location information in both Arabic and English.

--- types/city.ts
+++ types/city.ts
@@ -1,12 +1,14 @@
 export interface Region {
     id: number;
     name: string;
+    nameAr: string;
     cities: City[];
 }
 
 export interface City {
     id: number;
     name: string;
+    nameAr: string;
     regionId: number;
     region?: Region;
 }
